71c2273f80789305cceb5e5a9f4907fd38620504
BNargiz <nargiz493@gmail.com>
2022-05-02 14:41:10 +0200

    created seed in models

---
+++ seeders/20220502123624-some-categories.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("categories", [
+      { title: "Electronics", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Jewelery", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Men's Clothing", createdAt: new Date(), updatedAt: new Date() },
+      {
+        title: "Women's Clothing",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("categories", null, {});
+  },
+};
